26659690c1d58bf2f8ce4988241ecd4516fa2197
Sharad Banga <sharadiit03@gmail.com>
2025-01-26 09:34:49 +0530

    update

--- vaultX/src/App.jsx
+++ vaultX/src/App.jsx
@@ -9,6 +9,8 @@ import {
     WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
+import fs from "fs";
+
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
@@ -17,9 +19,15 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      hi there
-    </>
+    <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                  <>
+                    hi there
+                  </>
+            </WalletModalProvider>
+        </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
