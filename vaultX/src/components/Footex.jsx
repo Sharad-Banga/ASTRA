d94091e5a1f50bc0e9fe17e21d5862c6e8f917d4
Sharad Banga <sharadiit03@gmail.com>
2025-02-01 17:26:22 +0530

    update

---
+++ vaultX/src/components/Footex.jsx
@@ -0,0 +1,5 @@
+export const Footer = ()=>{
+        <div className="footer">
+            Developed and Designed By <a className="footer-link" href="https://x.com/sharad_banga" target="_blank" rel="noopener noreferrer">&nbsp;  sharad banga</a>
+        </div>
+}
